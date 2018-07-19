34c0958a67df4ccd615204adcbceb9a16c5ff543
Olli Ahonen <olliahonen>
2018-07-19 22:33:01 +0300

    Add convenience `from` function

--- src/PromiseEither.ts
+++ src/PromiseEither.ts
@@ -4,4 +4,8 @@ export class PromiseEither<L, A> {
   constructor(readonly value: Promise<Either<L, A>>) {
   }
 
+  public static from<K, T>(e: Either<K, T>): PromiseEither<K, T> {
+    return new PromiseEither(Promise.resolve(e))
+  }
+
 }
